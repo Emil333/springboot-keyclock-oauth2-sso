0af34668faca476d791614cea210e13c667ee8ef
Emil Pradeep <emilpradeep@gmail.com>
2020-11-27 11:13:23 +0530

    keyclock and google oauth2 implemented, multi tenant support also added

--- sso-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
+++ sso-resource-server/src/main/java/com/baeldung/resource/spring/SecurityConfig.java
@@ -4,24 +4,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {// @formatter:off
+//        Jwt
+        System.out.println("<------------------------Entered authenticator--------------->");
+        JwtIssuerAuthenticationManagerResolver authenticationManagerResolver = new JwtIssuerAuthenticationManagerResolver(
+                "http://localhost:8080/auth/realms/SpringBootKeyClock",
+                "https://accounts.google.com/o/oauth2/auth",
+                "https://dev-4482561.okta.com/oauth2/default"
+        );
+
         http.cors()
-            .and()
-              .authorizeRequests()
+                .and()
+                .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/user/info", "/api/foos/**")
-                  .hasAuthority("SCOPE_email")
+                .hasAnyAuthority("SCOPE_email")
                 .antMatchers(HttpMethod.POST, "/api/foos")
-                  .hasAuthority("SCOPE_profile")
+                .hasAuthority("SCOPE_profile")
                 .anyRequest()
-                  .authenticated()
-            .and()
-              .oauth2ResourceServer()
-                .jwt();
+                .authenticated()
+                .and()
+                .oauth2ResourceServer(oauth2 -> oauth2.authenticationManagerResolver(authenticationManagerResolver));
     }//@formatter:on
 
 }
